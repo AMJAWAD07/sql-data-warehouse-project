77a84ca73aff2d6249fb5a6932af04872cc75f7b
AMJAWAD07 <ap.mohammedjawad@gmail.com>
2025-07-30 18:08:38 +0530

    Update 03_date_range_exploration.sql

--- scripts/Exploratory_Data_Analytics/03_date_range_exploration.sql
+++ scripts/Exploratory_Data_Analytics/03_date_range_exploration.sql
@@ -1,1 +1,28 @@
+/*
+===============================================================================
+Date Range Exploration 
+===============================================================================
+Purpose:
+    - To determine the temporal boundaries of key data points.
+    - To understand the range of historical data.
 
+SQL Functions Used:
+    - MIN(), MAX(), DATEDIFF()
+===============================================================================
+*/
+
+-- Determine the first and last order date and the total duration in months
+select 
+    min(order_date) as first_order_date,
+    max(order_date) as last_order_date,
+    timestampdiff(month, min(order_date), max(order_date)) as order_range_months
+from gold_fact_sales;
+
+
+-- Find the youngest and oldest customer based on birthdate
+select
+    MIN(birthdate) AS oldest_birthdate,
+    timestampdiff(year, MIN(birthdate), now()) AS oldest_age,
+    MAX(birthdate) AS youngest_birthdate,
+    timestampdiff(year, MAX(birthdate), now()) AS youngest_age
+FROM gold_dim_customers;
