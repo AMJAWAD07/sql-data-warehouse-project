826bf67acb788aa968a02dff10f16873619d8e93
AMJAWAD07 <ap.mohammedjawad@gmail.com>
2025-07-30 18:04:09 +0530

    Update 02_dimensions_exploration.sql

--- scripts/Exploratory_Data_Analytics/02_dimensions_exploration.sql
+++ scripts/Exploratory_Data_Analytics/02_dimensions_exploration.sql
@@ -1,1 +1,27 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+select distinct 
+    country 
+from gold_dim_customers
+order by country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+select distinct 
+    category, 
+    subcategory, 
+    product_name 
+from gold_dim_products
+order by category, subcategory, product_name;
 
