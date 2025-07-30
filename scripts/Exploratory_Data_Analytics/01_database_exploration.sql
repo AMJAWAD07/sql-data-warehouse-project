11f05dfe53b2012d94f1123587a6d6a1a1532414
AMJAWAD07 <ap.mohammedjawad@gmail.com>
2025-07-30 17:56:32 +0530

    Update 01_database_exploration.sql

--- scripts/Exploratory_Data_Analytics/01_database_exploration.sql
+++ scripts/Exploratory_Data_Analytics/01_database_exploration.sql
@@ -1,1 +1,29 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
 
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+-- Retrieve a list of all tables in the database
+select 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+from INFORMATION_SCHEMA.TABLES;
+
+-- Retrieve all columns for a specific table (gold_dim_customers)
+select 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
+from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME = 'gold_dim_customers';
